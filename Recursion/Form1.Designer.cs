90dde8857f7e398f870ef21ae1099efa9313d236
Jken30 <emnacejasper@gmail.com>
2025-02-07 18:12:38 +0800

    Done Recursion

--- Recursion/Form1.Designer.cs
+++ Recursion/Form1.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnCalculateSum = new Button();
+            txtArrayInput = new TextBox();
+            lblSumResult = new Label();
+            CalculateSum = new Button();
+            SuspendLayout();
+            // 
+            // btnCalculateSum
+            // 
+            btnCalculateSum.Location = new Point(51, 91);
+            btnCalculateSum.Name = "btnCalculateSum";
+            btnCalculateSum.Size = new Size(112, 23);
+            btnCalculateSum.TabIndex = 0;
+            btnCalculateSum.Text = "Calculate Factorial";
+            btnCalculateSum.UseVisualStyleBackColor = true;
+            btnCalculateSum.Click += btnCalculateSum_Click;
+            // 
+            // txtArrayInput
+            // 
+            txtArrayInput.Location = new Point(235, 91);
+            txtArrayInput.Multiline = true;
+            txtArrayInput.Name = "txtArrayInput";
+            txtArrayInput.Size = new Size(186, 235);
+            txtArrayInput.TabIndex = 1;
+            // 
+            // lblSumResult
+            // 
+            lblSumResult.AutoSize = true;
+            lblSumResult.Location = new Point(233, 67);
+            lblSumResult.Name = "lblSumResult";
+            lblSumResult.Size = new Size(7, 15);
+            lblSumResult.TabIndex = 2;
+            lblSumResult.Text = "\r\n";
+            // 
+            // CalculateSum
+            // 
+            CalculateSum.Location = new Point(51, 143);
+            CalculateSum.Name = "CalculateSum";
+            CalculateSum.Size = new Size(112, 23);
+            CalculateSum.TabIndex = 3;
+            CalculateSum.Text = "Calculate Sum";
+            CalculateSum.UseVisualStyleBackColor = true;
+            CalculateSum.Click += CalculateSum_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(498, 382);
+            Controls.Add(CalculateSum);
+            Controls.Add(lblSumResult);
+            Controls.Add(txtArrayInput);
+            Controls.Add(btnCalculateSum);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnCalculateSum;
+        private TextBox txtArrayInput;
+        private Label lblSumResult;
+        private Button CalculateSum;
     }
 }
--- Recursion/Form1.cs
+++ Recursion/Form1.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace Recursion
 {
     public partial class Form1 : Form
@@ -5,6 +7,40 @@ namespace Recursion
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public int RecursiveFactorial(int n)
+        {
+            if (n == 0) return 1;
+            return n * RecursiveFactorial(n - 1);
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculateSum_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(txtArrayInput.Text);
+            int result = RecursiveFactorial(number);
+            lblSumResult.Text = $"Factorial: {result}";
+
+        }
+
+        public int RecursiveSum(int[] numbers, int n)
+        {
+            if (n == 0) return numbers[0];
+            return numbers[n] + RecursiveSum(numbers, n - 1);
+
+
+        }
+
+        private void CalculateSum_Click(object sender, EventArgs e)
+        {
+            int[] numbers = txtArrayInput.Text.Split(' ').Select(int.Parse).ToArray();
+            int result = RecursiveSum(numbers, numbers.Length -1);
+            lblSumResult.Text = $"Sum: {result}";
         }
     }
 }

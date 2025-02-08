1bc6a504a438631e853c6ca47b71d0efa1bb2313
Jken30 <emnacejasper@gmail.com>
2025-02-08 15:12:36 +0800

    Done Changes

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -20,20 +20,41 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ShowInfo = new Button();
+            SuspendLayout();
+            // 
+            // ShowInfo
+            // 
+            ShowInfo.Location = new Point(52, 71);
+            ShowInfo.Name = "ShowInfo";
+            ShowInfo.Size = new Size(75, 23);
+            ShowInfo.TabIndex = 0;
+            ShowInfo.Text = "Show";
+            ShowInfo.UseVisualStyleBackColor = true;
+            ShowInfo.Click += ShowInfo_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(193, 172);
+            Controls.Add(ShowInfo);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private Button ShowInfo;
+    }
 }
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -1,10 +1,78 @@
+using System.Reflection;
+
 namespace lab3
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Book
+        {
+            public string Title { get; set; }
+            public string Author { get; set; }
+
+            public virtual string GetInfo()
+            {
+                return $"{Title} by {Author}";
+            }
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+
+
+
+        public class Magazine : Book
+        {
+            public int IssueNumber { get; set; }
+            public override string GetInfo()
+            {
+                return $"{Title} by {Author}, Issue {IssueNumber}";
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void ShowInfo_Click(object sender, EventArgs e)
+        {
+
+            Book mybook = new Magazine {
+                Title = "Teck Weekly",
+                Author = "John Doe", IssueNumber = 45
+
+            };
+
+            MessageBox.Show(mybook.GetInfo());
+        }
+    }
 }
